ac054535dc3567ab1e0020bd7abbc2d931e9967c
Naveen_T <99719238+NaVeeNThiYaGu@users.noreply.github.com>
2024-01-07 16:19:57 +0530

    Add files via upload

---
+++ src/components/project/ProjectData.jsx
@@ -0,0 +1,28 @@
+import netflix from "../../assets/netflix.svg";
+import pizza from "../../assets/pizza.svg";
+import water from "../../assets/water.svg";
+
+const projectData = [
+    {
+        title : "Pizza Ordering Application",
+        description : "Web app which helps users to order pizzas and customize their pizzas.",
+        image : pizza,
+        projectLink : "https://github.com/NaVeeNThiYaGu/Pizza-Project",
+    },
+    {
+        title : "Netflix Clone",
+        description : "web app mimicking the renowned streaming service providing users with a seamless interface to explore, search, and enjoy movies and TV shows.",
+        image : netflix,
+        projectLink : "https://github.com/NaVeeNThiYaGu/Netflilx_Clone",
+    },
+    {
+        title : "Efficient Water Quality Analysis and Prediction",
+        description : "Machine learning model for predicting water quality.",
+        image : water,
+        projectLink : "https://github.com/NaVeeNThiYaGu/Water-Quality-Analysis-prediction",
+    },
+    
+  
+];
+
+export default projectData;
